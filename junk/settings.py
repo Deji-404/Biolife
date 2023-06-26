a7cad13ed33d003ca171404f6f8304865e7b6054
kentsam <kentsam262@gmail.com>
2023-06-26 12:28:23 +0100

    static file and gunicorn

--- junk/settings.py
+++ junk/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,9 +143,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:
+
+    STATIC_ROOT = BASE_DIR / 'static'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static"
-]
+] 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
--- junk/wsgi.py
+++ junk/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'junk.settings')
 
 application = get_wsgi_application()
+
+#gunicorn -w 4 junk.wsgi:application
